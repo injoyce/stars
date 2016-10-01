efa925a142548fe9c74c6ab4cf313471abab004d
injoyce <zhangpingping891@gmail.com>
2016-10-01 16:31:25 +0900

    homewor

--- Fizz buzz问题.rb
+++ Fizz buzz问题.rb
@@ -1,4 +1,4 @@
 ＃Fizz buzz问题
-
+＃practise
 
 upto(100){ ｜n｜ puts n%3 ==0 ? n%5 == 0 ? "fizzbuzz":"buzz": n%5 == 0 ? "fizz":n}
--- 闰年问题.rb
+++ 闰年问题.rb
@@ -1,4 +1,5 @@
 ＃闰年问题
+＃practise
 
 def IsLeapYear(y)
     if ((y%4 == 0)and (y%100!=0)) or (y%400==0)
